bc6684ef8c0f6b4111cfed97106ba5bc6360c9ae
Yohann DE <yohann.degliesposti@gmail.com>
2020-12-05 12:10:38 +0100

    bezier: now following a bezier curve !

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public static GameController activeGC;
+    private void Awake()
+    {
+        if (activeGC)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            activeGC = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/QTEs/QTEController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QTEController : MonoBehaviour
+{
+    public List<GameObject> activeQTEs;
+
+    public GameObject qteSliderPrefab;
+    public GameObject qteClickPrefab;
+    public GameObject qteTouchPrefab;
+
+    public enum QTEType
+    {
+        SLIDER,
+        CLICK,
+        TOUCH
+    }
+
+    public void createQTE(QTEType type)
+    {
+        switch(type)
+        {
+            case QTEType.SLIDER:
+                activeQTEs.Add(Instantiate(qteSliderPrefab));
+                break;
+            case QTEType.CLICK:
+                activeQTEs.Add(Instantiate(qteClickPrefab));
+                break;
+            case QTEType.TOUCH:
+                activeQTEs.Add(Instantiate(qteTouchPrefab));
+                break;
+        }
+    }
+}
