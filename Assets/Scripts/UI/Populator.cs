0c063b738749ce853163147970a273701d49d868
Yohann DE <yohann.degliesposti@gmail.com>
2020-12-06 17:34:35 +0100

    pokedex: now working !

---
+++ Assets/Scripts/UI/Populator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Populator : MonoBehaviour
+{
+    public Sprite incognitoSprite;
+    public GameObject prefabEntry;
+
+    public GameObject content;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        populateTable();
+    }
+
+    private void populateTable()
+    {
+        foreach (Gifts g in GameController.activeGC.allAvailableGifts)
+        {
+            GameObject prefab = Instantiate(prefabEntry);
+            prefab.transform.SetParent(content.transform);
+            if (GameController.activeGC.discoveredGifts.Contains(g.nam))
+                prefab.GetComponent<PrefabEntryItem>().INCARNATE(g.nam, g.tagline, g.image);
+            else
+            {
+                if (g.objectState == Gifts.state.NORMAL)
+                    prefab.GetComponent<PrefabEntryItem>().INCARNATE("????", "Play in normal to discover that gift !", incognitoSprite);
+                else if (g.objectState == Gifts.state.HARD)
+                    prefab.GetComponent<PrefabEntryItem>().INCARNATE("????", "Play in hard to discover that gift !", incognitoSprite);
+                else
+                    prefab.GetComponent<PrefabEntryItem>().INCARNATE("????", "Play in normal or hard to discover that gift !", incognitoSprite);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PrefabEntryItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrefabEntryItem : MonoBehaviour
+{
+    public Image spriterender;
+    public TextMeshProUGUI title;
+    public TextMeshProUGUI line;
+
+    public void INCARNATE(string titleline, string descr, Sprite sprite)
+    {
+        spriterender.sprite = sprite;
+        title.text = titleline;
+        line.text = descr;
+    }
+}
